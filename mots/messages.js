c94ca7065ca5d0661f4c52923d4b1ec07febca2a
Didier Caramello <dcaramello@bbox.fr>
2026-06-08 16:01:46 +0000

    Ajouter un fichier

---
+++ mots/messages.js
@@ -0,0 +1,8 @@
+const MESSAGE_INITIAL = [``, "20260125", "20260125"];
+const MESSAGE_FINAL = [`<div style=' text-align:center;'><span style='color:#e7002a; font-size:1.6rem; font-weight:bold;'Ce samedi, Motchus s'invite à l'Alcazar !</span><br> À 17h à la bibliothèque de Marseille, Médéric Gasquet-Cyrus anime un atelier du parler marseillais et vous fait jouer à Motchus, en groupe !</div>`, "20260515", "20260516"];
+const MESSAGE_FINALtypemotchus = [`<div style=' text-align:center;'><span style='color:#e7002a; font-size:1.6rem; font-weight:bold;'>Spécial Saint-Valentin</span><br>Trouvez 20 mots marseillais en lien avec la Saint-Valentin !<br><a href='valentinvalentchine/'>https://motchus.fr/valentinvalentchine</a></div>`, "20260101", "20260101"];
+const MESSAGE_FINALtypeliens = [`
+            <h3>Série spéciale</h3>
+            <div class="liens-grid" style="grid-template-columns: none; max-width:100%; margin:0; padding:0;">
+                <a class="lien-carte" href="henri-blanc/" style="text-align:left;" >Trouvez 20 mots d'Henri-Frédéric Blanc</a>
+            </div>`, "20260125", "20260205"];
