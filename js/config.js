f64f179b5a4fc298ef4292e0dc9f1aee5b53e8c5
Didier Caramello <dcaramello@bbox.fr>
2026-06-08 15:25:24 +0000

    Ajouter un fichier

---
+++ js/config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+    maxEssais: 6,
+    seuilOffrePremiere: 6,
+    dureeMessage: 4000,
+    dureeMessageAide: 20000,
+    dureeMessageInitial: 300000,
+    texteGagne: "Aïoli !",
+    textePerdu: "Perdu ! Marque-mal !",
+    texteTropCourt : "Le mot proposé est trop court.",
+    textePasDico : "Le mot n'est pas dans le dictionnaire Motchus.",
+    texteprefixAide:"<i>Tu t'en sors ou on va chercher Molinàri ?</i><br>Indice : ",
+};
+
+
+
