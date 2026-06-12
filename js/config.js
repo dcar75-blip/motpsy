2948b2bc6aeb23b8d26f345cea70a1d329813f08
dcar75-blip <dcar75@gmail.com>
2026-06-12 18:03:55 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,10 +4,10 @@ const CONFIG = {
     dureeMessage: 4000,
     dureeMessageAide: 20000,
     dureeMessageInitial: 300000,
-    texteGagne: "Aïoli !",
-    textePerdu: "Perdu ! Marque-mal !",
-    texteTropCourt : "Le mot proposé est trop court.",
-    textePasDico : "Le mot n'est pas dans le dictionnaire Motchus.",
+texteGagne: "Bien joué !",
+textePerdu: "Perdu ! Une séance et ça ira mieux !",
+texteTropCourt : "Le mot proposé est trop court.",
+textePasDico : "Ce mot n'est pas dans notre dictionnaire.",
     texteprefixAide:"<i>Tu t'en sors ou on va chercher Molinàri ?</i><br>Indice : ",
 };
 
